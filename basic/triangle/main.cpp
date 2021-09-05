fbee7c99e1032d99ce5f82650d8290e84164e293
gandis <gandis0713@gmail.com>
2021-09-05 18:50:19 +0900

    feat: implemented that creating image view

--- basic/triangle/main.cpp
+++ basic/triangle/main.cpp
@@ -82,6 +82,8 @@ private:
     VkFormat m_swapChainImageFormat;
     VkExtent2D m_swapChainExtent;
 
+    std::vector<VkImageView> m_vecSwapChainImageViews;
+
 private:
     void initWindow() {
         glfwInit();
@@ -99,6 +101,7 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
     }
 
     void mainLoop() {
@@ -108,6 +111,10 @@ private:
     }
 
     void cleanup() {
+
+        for (const VkImageView& imageView : m_vecSwapChainImageViews) {
+            vkDestroyImageView(m_logicalDevice, imageView, nullptr);
+        }
 
         vkDestroySwapchainKHR(m_logicalDevice, m_swapChain, nullptr);
 
@@ -566,6 +573,34 @@ private:
 
         m_swapChainImageFormat = surfaceFormat.format;
         m_swapChainExtent = imageExtent;
+    }
+
+    void createImageViews() {
+        m_vecSwapChainImageViews.resize(m_vecSwapChainImages.size());
+
+        for (size_t i = 0; i < m_vecSwapChainImages.size(); i++) {
+            VkImageViewCreateInfo imageViewCreateInfo{};
+            imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            imageViewCreateInfo.image = m_vecSwapChainImages[i];
+            imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            imageViewCreateInfo.format = m_swapChainImageFormat;
+
+            imageViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            imageViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            imageViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            imageViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+            imageViewCreateInfo.subresourceRange.levelCount = 1;
+            imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+            imageViewCreateInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(m_logicalDevice, &imageViewCreateInfo, nullptr, &m_vecSwapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }
+
     }
 
 };
