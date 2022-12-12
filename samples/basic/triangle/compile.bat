e0b145fdab65270efe985652a55f6f9e96ff3766
gandis0713 <gandis0713@gmail.com>
2022-12-12 22:40:14 +0900

    compile shader in cmake

--- samples/basic/triangle/compile.bat
+++
@@ -1,5 +0,0 @@
-del vert.spv
-del frag.spv
-
-call %VULKAN_SDK%/bin/glslc %~dp0/shader.vert -o %~dp0/vert.spv
-call %VULKAN_SDK%/bin/glslc %~dp0/shader.frag -o %~dp0/frag.spv
--- samples/basic/triangle/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-SPV_DIR=$(pwd)/$(dirname "$0")
-
-rm -f ${SPV_DIR}/vert.spv
-rm -f ${SPV_DIR}/frag.spv
-
-$VULKAN_SDK/bin/glslc ${SPV_DIR}/shader.vert -o ${SPV_DIR}/vert.spv
-$VULKAN_SDK/bin/glslc ${SPV_DIR}/shader.frag -o ${SPV_DIR}/frag.spv
