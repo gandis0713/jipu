3746bda7faf26011ea428b053f6715e1e4a39f96
gandis0713 <gandis0713@gmail.com>
2023-01-08 18:56:09 +0900

    feat: check enumeratte instance extensions for portability

--- src/vk/driver.cpp
+++ src/vk/driver.cpp
@@ -1,6 +1,8 @@
 #include "driver.h"
 #include "allocation.h"
 
+#include <vector>
+
 namespace vkt
 {
 
@@ -8,12 +10,28 @@ Driver::Driver(Platform* platform) : m_platform(*platform)
 {
     // TODO: Load Vulkan entry points.
 
+    unsigned int extPropCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extPropCount, nullptr);
+
+    std::vector<VkExtensionProperties> extProps(extPropCount);
+    vkEnumerateInstanceExtensionProperties(static_cast<const char*>(nullptr),
+                                           &extPropCount, extProps.data());
+
+    bool portabilityEnumerationSupport = false;
+    for (const VkExtensionProperties& extProp : extProps)
+    {
+        if (!strcmp(extProp.extensionName,
+                    VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME))
+            portabilityEnumerationSupport = true;
+    }
+
     // Create Vulkan instance.
     VkInstanceCreateInfo instanceCreateInfo{};
     instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 #if defined(__APPLE__)
-    instanceCreateInfo.flags |=
-        VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    if (portabilityEnumerationSupport)
+        instanceCreateInfo.flags |=
+            VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 #endif
 
     instanceCreateInfo.enabledExtensionCount = 0;
