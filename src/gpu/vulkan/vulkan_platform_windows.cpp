924c4a38e04e8502e135921713129fa0a79604e2
gandis <gandis0713@gmail.com>
2023-03-20 18:43:39 +0900

    feat: implement platform for windows

--- src/gpu/vulkan/vulkan_platform_windows.cpp
+++ src/gpu/vulkan/vulkan_platform_windows.cpp
@@ -2,11 +2,30 @@
 
 #include "vulkan_adapter.h"
 
+#include <stdexcept>
+#include <fmt/format.h>
+
 namespace vkt
 {
 
 VulkanPlatformWindows::VulkanPlatformWindows(VulkanAdapter* adapter, PlatformCreateInfo info) noexcept : VulkanPlatform(adapter, info) {}
 
-VkSurfaceKHR VulkanPlatformWindows::createSurfaceKHR(SurfaceCreateInfo info) { return nullptr; }
+VkSurfaceKHR VulkanPlatformWindows::createSurfaceKHR(SurfaceCreateInfo info)
+{
+    VkWin32SurfaceCreateInfoKHR createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+    createInfo.hwnd = (HWND)m_windowHandle;
+    createInfo.hinstance = GetModuleHandle(nullptr);
+
+    VkSurfaceKHR surface{};
+    VulkanAdapter* adapter = static_cast<VulkanAdapter*>(m_adapter);
+    VkResult result = vkCreateWin32SurfaceKHR(adapter->getInstance(), &createInfo, nullptr, &surface);
+    if (result != VK_SUCCESS)
+    {
+        throw std::runtime_error(fmt::format("Failed to create VkSurfaceKHR.: {}", result));
+    }
+
+    return surface;
+}
 
 } // namespace vkt
