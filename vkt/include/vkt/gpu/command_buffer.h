2984afa7c73727a0725033363d4028aa1f67c15a
gandis0713 <gandis0713@gmail.com>
2023-05-14 20:59:10 +0900

    feat: remove commands file

--- vkt/include/vkt/gpu/command_buffer.h
+++ vkt/include/vkt/gpu/command_buffer.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include "vkt/gpu/buffer.h"
+
 namespace vkt
 {
+
+enum class CommandType
+{
+    kIndexBuffer,
+    kVertexBuffer,
+    kCopyBufferToBuffer,
+    kCopyBufferToTexture,
+};
+
+struct CopyBufferToBuffer
+{
+    Buffer* src;
+    Buffer* dst;
+    uint64_t srcOffset;
+    uint64_t dstOffset;
+    uint64_t size;
+};
 
 struct CommandBufferDescriptor
 {
