68cc9ce447ef9e9d2837fdd1da020a73922fd59e
gandis0713 <gandis0713@gmail.com>
2023-06-22 23:19:21 +0900

    define uniform buffer object

--- sample/apps/triangle/main.cpp
+++ sample/apps/triangle/main.cpp
@@ -41,6 +41,13 @@ private:
     void draw() override;
 
 private:
+    struct UniformBufferObject
+    {
+        glm::mat4 model;
+        glm::mat4 view;
+        glm::mat4 projection;
+    };
+
     struct Vertex
     {
         glm::vec2 pos;
