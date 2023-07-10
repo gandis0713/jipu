51434d79dbc9ac114782bbb3e3583dc0fe88821c
gandis0713 <gandis0713@gmail.com>
2023-07-10 21:01:44 +0900

    add command buffer usage

--- vkt/gpu/command_buffer.cpp
+++ vkt/gpu/command_buffer.cpp
@@ -6,7 +6,13 @@ namespace vkt
 
 CommandBuffer::CommandBuffer(Device* device, const CommandBufferDescriptor& descriptor)
     : m_device(device)
+    , m_descriptor(descriptor)
 {
+}
+
+CommandBufferUsage CommandBuffer::getUsage() const
+{
+    return m_descriptor.usage;
 }
 
 Device* CommandBuffer::getDevice() const
