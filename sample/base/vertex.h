8ffbed0ccd969b7844ec51aeae803cc8ab9971fe
gandis0713 <gandis0713@gmail.com>
2023-07-28 01:09:33 +0900

    remove header

--- sample/base/vertex.h
+++ sample/base/vertex.h
@@ -3,7 +3,6 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_ENABLE_EXPERIMENTAL
-#include <functional>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/hash.hpp>
