ae1d764afaa2d446746fd7e5e4d0d4e7b1cfd389
gandis0713 <gandis0713@gmail.com>
2023-08-15 09:21:35 +0900

    create device

--- sample/apps/compute/main.cpp
+++ sample/apps/compute/main.cpp
@@ -1,6 +1,13 @@
 
 #include "sample.h"
 
+#include "vkt/gpu/buffer.h"
+#include "vkt/gpu/device.h"
+#include "vkt/gpu/driver.h"
+#include "vkt/gpu/physical_device.h"
+#include "vkt/gpu/surface.h"
+#include "vkt/gpu/swapchain.h"
+
 #include <spdlog/spdlog.h>
 
 #if defined(__ANDROID__) || defined(ANDROID)
@@ -31,6 +38,21 @@ public:
 public:
     void init() override;
     void draw() override;
+
+private:
+    void createDriver();
+    void createPhysicalDevice();
+    void createDevice();
+
+private:
+    std::unique_ptr<Buffer> m_vertexBuffer = nullptr;
+    std::unique_ptr<Buffer> m_indexBUffer = nullptr;
+
+    std::unique_ptr<Driver> m_driver = nullptr;
+    std::unique_ptr<PhysicalDevice> m_physicalDevice = nullptr;
+    std::unique_ptr<Device> m_device = nullptr;
+    std::unique_ptr<Surface> m_surface = nullptr;
+    std::unique_ptr<Swapchain> m_swapchain = nullptr;
 };
 
 ComputeSample::ComputeSample(const SampleDescriptor& descriptor)
@@ -44,10 +66,37 @@ ComputeSample::~ComputeSample()
 
 void ComputeSample::init()
 {
+    createDriver();
+    createPhysicalDevice();
+    createDevice();
 }
 
 void ComputeSample::draw()
 {
+}
+
+void ComputeSample::createDriver()
+{
+    DriverDescriptor descriptor{ .type = DriverType::VULKAN };
+    m_driver = Driver::create(descriptor);
+}
+
+void ComputeSample::createPhysicalDevice()
+{
+    if (!m_driver)
+        throw std::runtime_error("The driver instance is null pointer.");
+
+    PhysicalDeviceDescriptor descriptor{ .index = 0 }; // TODO: use first physical device.
+    m_physicalDevice = m_driver->createPhysicalDevice(descriptor);
+}
+
+void ComputeSample::createDevice()
+{
+    if (!m_physicalDevice)
+        throw std::runtime_error("The physical device instance is null pointer.");
+
+    DeviceDescriptor descriptor{};
+    m_device = m_physicalDevice->createDevice(descriptor);
 }
 
 } // namespace vkt
