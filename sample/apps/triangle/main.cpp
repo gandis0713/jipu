3faf793c32c64655d0a9cb739e1e93eb302ad3da
gandis0713 <gandis0713@gmail.com>
2023-10-14 18:04:46 +0900

    create instance for vkt

--- sample/apps/triangle/main.cpp
+++ sample/apps/triangle/main.cpp
@@ -1,6 +1,12 @@
 
 
 #include "sample.h"
+
+#include "vkt/gpu/device.h"
+#include "vkt/gpu/driver.h"
+#include "vkt/gpu/physical_device.h"
+#include "vkt/gpu/surface.h"
+
 #include <spdlog/spdlog.h>
 
 #if defined(__ANDROID__) || defined(ANDROID)
@@ -30,6 +36,18 @@ public:
 
     void init() override;
     void draw() override;
+
+private:
+    void createDevier();
+    void createPhysicalDevice();
+    void createSurface();
+    void createDevice();
+
+private:
+    std::unique_ptr<Driver> m_driver = nullptr;
+    std::unique_ptr<PhysicalDevice> m_physicalDevice = nullptr;
+    std::unique_ptr<Surface> m_surface = nullptr;
+    std::unique_ptr<Device> m_device = nullptr;
 };
 
 TriangleSample::TriangleSample(const SampleDescriptor& descriptor)
@@ -39,14 +57,53 @@ TriangleSample::TriangleSample(const SampleDescriptor& descriptor)
 
 TriangleSample::~TriangleSample()
 {
+    m_device.reset();
+    m_surface.reset();
+    m_physicalDevice.reset();
+    m_driver.reset();
 }
 
 void TriangleSample::init()
 {
+    createDevier();
+    createPhysicalDevice();
+    createSurface();
+    createDevice();
 }
 
 void TriangleSample::draw()
 {
+}
+
+void TriangleSample::createDevier()
+{
+    DriverDescriptor descriptor{};
+    descriptor.type = DriverType::VULKAN;
+
+    m_driver = Driver::create(descriptor);
+}
+
+void TriangleSample::createPhysicalDevice()
+{
+    PhysicalDeviceDescriptor descriptor{};
+    descriptor.index = 0; // TODO: select device.
+
+    m_physicalDevice = m_driver->createPhysicalDevice(descriptor);
+}
+
+void TriangleSample::createDevice()
+{
+    SurfaceDescriptor descriptor{};
+    descriptor.windowHandle = getWindowHandle();
+
+    m_surface = m_driver->createSurface(descriptor);
+}
+
+void TriangleSample::createSurface()
+{
+    DeviceDescriptor descriptor{};
+
+    m_device = m_physicalDevice->createDevice(descriptor);
 }
 
 } // namespace vkt
