8511396a161904da9329f6ea8639b57af32cdf07
gandis0713 <gandis0713@gmail.com>
2023-10-27 13:51:10 +0900

    remove imgui code

--- sample/apps/imgui/main.cpp
+++ sample/apps/imgui/main.cpp
@@ -1,21 +1,8 @@
 
 
 #include "file.h"
+#include "gui.h"
 #include "sample.h"
-
-#include <imgui.h>
-
-#include "vkt/gpu/buffer.h"
-#include "vkt/gpu/command_buffer.h"
-#include "vkt/gpu/command_encoder.h"
-#include "vkt/gpu/device.h"
-#include "vkt/gpu/driver.h"
-#include "vkt/gpu/physical_device.h"
-#include "vkt/gpu/pipeline.h"
-#include "vkt/gpu/pipeline_layout.h"
-#include "vkt/gpu/queue.h"
-#include "vkt/gpu/surface.h"
-#include "vkt/gpu/swapchain.h"
 
 #include <glm/glm.hpp>
 #include <spdlog/spdlog.h>
@@ -38,7 +25,7 @@ extern "C"
 namespace vkt
 {
 
-class ImGuiSample : public Sample
+class ImGuiSample : public Sample, public GUI
 {
 public:
     ImGuiSample() = delete;
@@ -49,10 +36,7 @@ public:
     void draw() override;
 
 private:
-    void initImGui();
-    void updateImGui();
-    void drawImGui(RenderPassEncoder* renderPassEncoder);
-    void clearImGui();
+    void updateImGui() override;
 
 private:
     void createDevier();
@@ -136,15 +120,15 @@ void ImGuiSample::init()
     createVertexBuffer();
     createRenderPipeline();
 
-    initImGui();
+    initImGui(m_device.get(), m_queue.get(), m_swapchain.get());
 }
 
 void ImGuiSample::draw()
 {
+    updateImGui();
+    updateImGuiBuffer();
 
     auto swapchainIndex = m_swapchain->acquireNextTexture();
-
-    updateImGui();
 
     {
         ColorAttachment attachment{};
@@ -176,229 +160,6 @@ void ImGuiSample::draw()
     }
 }
 
-void ImGuiSample::initImGui()
-{
-    // IMGUI_CHECKVERSION();
-    ImGuiContext* imguiContext = ImGui::CreateContext();
-    if (imguiContext == nullptr)
-    {
-        throw std::runtime_error("Failed to create imgui context");
-    }
-
-    auto scale = 1.0f;
-    ImGuiIO& io = ImGui::GetIO();
-    io.FontGlobalScale = scale;
-    io.DisplaySize = ImVec2(m_width, m_height);
-    io.DisplayFramebufferScale = ImVec2(1.0f, 1.0f);
-
-    ImGuiStyle& style = ImGui::GetStyle();
-    style.Colors[ImGuiCol_TitleBg] = ImVec4(1.0f, 0.0f, 0.0f, 0.6f);
-    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(1.0f, 0.0f, 0.0f, 0.8f);
-    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(1.0f, 0.0f, 0.0f, 0.4f);
-    style.Colors[ImGuiCol_Header] = ImVec4(1.0f, 0.0f, 0.0f, 0.4f);
-    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
-
-    // Get texture for fonts.
-    using FontDataType = unsigned char;
-    FontDataType* fontData = nullptr;
-    int fontTexWidth, fontTexHeight;
-    io.Fonts->GetTexDataAsRGBA32(&fontData, &fontTexWidth, &fontTexHeight);
-
-    // create font texture.
-    {
-        TextureDescriptor fontTextureDescriptor{};
-        fontTextureDescriptor.type = TextureType::k2D;
-        fontTextureDescriptor.format = TextureFormat::kRGBA_8888_UInt_Norm;
-        fontTextureDescriptor.width = fontTexWidth;
-        fontTextureDescriptor.height = fontTexHeight;
-        fontTextureDescriptor.mipLevels = 1;
-        fontTextureDescriptor.sampleCount = 1;
-        fontTextureDescriptor.usage = TextureUsageFlagBits::kCopyDst |
-                                      TextureUsageFlagBits::kTextureBinding;
-
-        m_imguiResources.fontTexture = m_device->createTexture(fontTextureDescriptor);
-    }
-
-    // create font texture view.
-    {
-        TextureViewDescriptor fontTextureViewDescriptor{};
-        fontTextureViewDescriptor.aspect = TextureAspectFlagBits::kColor;
-        fontTextureViewDescriptor.type = TextureViewType::k2D;
-
-        m_imguiResources.fontTextureView = m_imguiResources.fontTexture->createTextureView(fontTextureViewDescriptor);
-    }
-
-    // create font staging buffer.
-    std::unique_ptr<Buffer> m_fontBuffer = nullptr;
-    {
-        BufferDescriptor fontBufferDescriptor{};
-        fontBufferDescriptor.size = fontTexWidth * fontTexHeight * 4 * sizeof(FontDataType);
-        fontBufferDescriptor.usage = BufferUsageFlagBits::kCopySrc;
-
-        m_fontBuffer = m_device->createBuffer(fontBufferDescriptor);
-
-        void* fontGPUMappedPointer = m_fontBuffer->map();
-        memcpy(fontGPUMappedPointer, fontData, fontBufferDescriptor.size);
-        // m_fontBuffer->unmap();
-    }
-
-    // copy buffer to texture
-    {
-        BlitTextureBuffer blitTextureBuffer{};
-        blitTextureBuffer.buffer = m_fontBuffer.get();
-        blitTextureBuffer.offset = 0;
-        uint32_t channel = 4;
-        uint32_t bytesPerData = sizeof(FontDataType);
-        blitTextureBuffer.bytesPerRow = bytesPerData * m_imguiResources.fontTexture->getWidth() * channel;
-        blitTextureBuffer.rowsPerTexture = m_imguiResources.fontTexture->getHeight();
-
-        BlitTexture blitTexture{ .texture = m_imguiResources.fontTexture.get() };
-        Extent3D extent{};
-        extent.width = m_imguiResources.fontTexture->getWidth();
-        extent.height = m_imguiResources.fontTexture->getHeight();
-        extent.depth = 1;
-
-        CommandBufferDescriptor commandBufferDescriptor{ .usage = CommandBufferUsage::kOneTime };
-        std::unique_ptr<CommandBuffer> commandBuffer = m_device->createCommandBuffer(commandBufferDescriptor);
-
-        CommandEncoderDescriptor commandEncoderDescriptor{};
-        std::unique_ptr<CommandEncoder> commandEncoder = commandBuffer->createCommandEncoder(commandEncoderDescriptor);
-        commandEncoder->copyBufferToTexture(blitTextureBuffer, blitTexture, extent);
-
-        m_queue->submit({ commandEncoder->finish() });
-    }
-
-    // create font sampler
-    {
-        SamplerDescriptor fontSamplerDescriptor{};
-        fontSamplerDescriptor.addressModeU = AddressMode::kClampToEdge;
-        fontSamplerDescriptor.addressModeV = AddressMode::kClampToEdge;
-        fontSamplerDescriptor.addressModeW = AddressMode::kClampToEdge;
-        fontSamplerDescriptor.lodMin = 0.0f;
-        fontSamplerDescriptor.lodMax = static_cast<uint32_t>(m_imguiResources.fontTexture->getMipLevels());
-        fontSamplerDescriptor.minFilter = FilterMode::kLinear;
-        fontSamplerDescriptor.magFilter = FilterMode::kLinear;
-        fontSamplerDescriptor.mipmapFilter = MipmapFilterMode::kLinear;
-
-        m_imguiResources.fontSampler = m_device->createSampler(fontSamplerDescriptor);
-    }
-
-    // create binding group layout
-    {
-        SamplerBindingLayout fontSamplerBindingLayout{};
-        fontSamplerBindingLayout.index = 0;
-        fontSamplerBindingLayout.stages = BindingStageFlagBits::kFragmentStage;
-        fontSamplerBindingLayout.withTexture = m_imguiResources.fontTextureView != nullptr;
-
-        BindingGroupLayoutDescriptor bindingGroupLayoutDescriptor{};
-        bindingGroupLayoutDescriptor.samplers = { fontSamplerBindingLayout };
-
-        m_imguiResources.bindingGroupLayout = m_device->createBindingGroupLayout(bindingGroupLayoutDescriptor);
-    }
-
-    // create binding group
-    {
-        SamplerBinding fontSamplerBinding{};
-        fontSamplerBinding.index = 0;
-        fontSamplerBinding.sampler = m_imguiResources.fontSampler.get();
-        fontSamplerBinding.textureView = m_imguiResources.fontTextureView.get();
-
-        BindingGroupDescriptor bindingGroupDescriptor{};
-        bindingGroupDescriptor.layout = m_imguiResources.bindingGroupLayout.get();
-        bindingGroupDescriptor.samplers = { fontSamplerBinding };
-
-        m_imguiResources.bindingGroup = m_device->createBindingGroup(bindingGroupDescriptor);
-    }
-
-    // create pipeline layout
-    {
-        PipelineLayoutDescriptor pipelineLayoutDescriptor{};
-        pipelineLayoutDescriptor.layouts = { m_imguiResources.bindingGroupLayout.get() };
-
-        m_imguiResources.pipelineLayout = m_device->createPipelineLayout(pipelineLayoutDescriptor);
-    }
-
-    // create pipeline
-    {
-        InputAssemblyStage inputAssemblyStage{};
-        {
-            inputAssemblyStage.topology = PrimitiveTopology::kTriangleList;
-        }
-
-        std::unique_ptr<ShaderModule> vertexShaderModule = nullptr;
-        VertexStage vertexStage{};
-        {
-
-            VertexInputLayout vertexInputLayout{};
-            {
-                VertexAttribute positionAttribute{};
-                positionAttribute.format = VertexFormat::kSFLOATx2;
-                positionAttribute.offset = offsetof(ImDrawVert, pos);
-
-                VertexAttribute uiAttribute{};
-                uiAttribute.format = VertexFormat::kSFLOATx2;
-                uiAttribute.offset = offsetof(ImDrawVert, uv);
-
-                VertexAttribute colorAttribute{};
-                colorAttribute.format = VertexFormat::kUNORM8x4;
-                colorAttribute.offset = offsetof(ImDrawVert, col);
-
-                vertexInputLayout.attributes = { positionAttribute, uiAttribute, colorAttribute };
-                vertexInputLayout.mode = VertexMode::kVertex;
-                vertexInputLayout.stride = sizeof(ImDrawVert);
-            }
-
-            {
-                std::vector<char> vertexShaderSource = utils::readFile(m_appDir / "imgui.vert.spv", m_handle);
-                ShaderModuleDescriptor vertexShaderModuleDescriptor{};
-                vertexShaderModuleDescriptor.code = vertexShaderSource.data();
-                vertexShaderModuleDescriptor.codeSize = static_cast<uint32_t>(vertexShaderSource.size());
-
-                vertexShaderModule = m_device->createShaderModule(vertexShaderModuleDescriptor);
-            }
-
-            vertexStage.entryPoint = "main";
-            vertexStage.layouts = { vertexInputLayout };
-            vertexStage.shaderModule = vertexShaderModule.get();
-        }
-
-        RasterizationStage rasterizationStage{};
-        {
-            rasterizationStage.cullMode = CullMode::kNone;
-            rasterizationStage.frontFace = FrontFace::kCounterClockwise;
-            rasterizationStage.sampleCount = 1;
-        }
-
-        std::unique_ptr<ShaderModule> fragmentShaderModule = nullptr;
-        FragmentStage fragmentStage{};
-        {
-            FragmentStage::Target target{};
-            target.format = m_swapchain->getTextureFormat();
-
-            {
-                std::vector<char> fragmentShaderSource = utils::readFile(m_appDir / "imgui.frag.spv", m_handle);
-                ShaderModuleDescriptor fragmentShaderModuleDescriptor{};
-                fragmentShaderModuleDescriptor.code = fragmentShaderSource.data();
-                fragmentShaderModuleDescriptor.codeSize = static_cast<uint32_t>(fragmentShaderSource.size());
-
-                fragmentShaderModule = m_device->createShaderModule(fragmentShaderModuleDescriptor);
-            }
-
-            fragmentStage.targets = { target };
-            fragmentStage.entryPoint = "main";
-            fragmentStage.shaderModule = fragmentShaderModule.get();
-        }
-
-        RenderPipelineDescriptor renderPipelineDescriptor{};
-        renderPipelineDescriptor.layout = m_imguiResources.pipelineLayout.get();
-        renderPipelineDescriptor.inputAssembly = inputAssemblyStage;
-        renderPipelineDescriptor.vertex = vertexStage;
-        renderPipelineDescriptor.rasterization = rasterizationStage;
-        renderPipelineDescriptor.fragment = fragmentStage;
-
-        m_imguiResources.pipeline = m_device->createRenderPipeline(renderPipelineDescriptor);
-    }
-}
 void ImGuiSample::updateImGui()
 {
     // set display size and mouse state.
@@ -427,116 +188,6 @@ void ImGuiSample::updateImGui()
         ImGui::End();
     }
     ImGui::Render();
-
-    // update buffer
-    {
-        ImDrawData* imDrawData = ImGui::GetDrawData();
-        uint32_t vertexBufferSize = imDrawData->TotalVtxCount * sizeof(ImDrawVert);
-        uint32_t indexBufferSize = imDrawData->TotalIdxCount * sizeof(ImDrawIdx);
-
-        if ((vertexBufferSize == 0) || (indexBufferSize == 0))
-        {
-            return;
-        }
-
-        // Update buffers only if vertex or index count has been changed compared to current buffer size
-
-        // Vertex buffer
-        if ((m_imguiResources.vertexBuffer == nullptr) || (m_imguiResources.vertexBuffer->getSize() != vertexBufferSize))
-        {
-            m_imguiResources.vertexBuffer.reset();
-
-            BufferDescriptor descriptor{};
-            descriptor.size = vertexBufferSize;
-            descriptor.usage = BufferUsageFlagBits::kVertex;
-
-            m_imguiResources.vertexBuffer = m_device->createBuffer(descriptor);
-            m_imguiResources.vertexBuffer->map();
-        }
-
-        // Index buffer
-        if ((m_imguiResources.indexBuffer == nullptr) || (m_imguiResources.indexBuffer->getSize() < indexBufferSize))
-        {
-            m_imguiResources.indexBuffer.reset();
-
-            BufferDescriptor descriptor{};
-            descriptor.size = indexBufferSize;
-            descriptor.usage = BufferUsageFlagBits::kIndex;
-
-            m_imguiResources.indexBuffer = m_device->createBuffer(descriptor);
-            m_imguiResources.indexBuffer->map();
-        }
-
-        // Upload data
-        ImDrawVert* vtxDst = (ImDrawVert*)m_imguiResources.vertexBuffer->map();
-        ImDrawIdx* idxDst = (ImDrawIdx*)m_imguiResources.indexBuffer->map();
-
-        for (int n = 0; n < imDrawData->CmdListsCount; n++)
-        {
-            const ImDrawList* cmd_list = imDrawData->CmdLists[n];
-            memcpy(vtxDst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
-            memcpy(idxDst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
-            vtxDst += cmd_list->VtxBuffer.Size;
-            idxDst += cmd_list->IdxBuffer.Size;
-        }
-
-        // Flush to make writes visible to GPU
-        // vertexBuffer.flush();
-        // indexBuffer.flush();
-    }
-}
-
-void ImGuiSample::drawImGui(RenderPassEncoder* renderPassEncoder)
-{
-    ImDrawData* imDrawData = ImGui::GetDrawData();
-    int32_t vertexOffset = 0;
-    int32_t indexOffset = 0;
-
-    ImGuiIO& io = ImGui::GetIO();
-
-    renderPassEncoder->setPipeline(m_imguiResources.pipeline.get());
-    renderPassEncoder->setBindingGroup(0, m_imguiResources.bindingGroup.get());
-    renderPassEncoder->setViewport(0, 0, ImGui::GetIO().DisplaySize.x, ImGui::GetIO().DisplaySize.y, 0, 1);
-
-    if (imDrawData->CmdListsCount > 0)
-    {
-        renderPassEncoder->setVertexBuffer(m_imguiResources.vertexBuffer.get());
-        renderPassEncoder->setIndexBuffer(m_imguiResources.indexBuffer.get());
-
-        for (int32_t i = 0; i < imDrawData->CmdListsCount; i++)
-        {
-            const ImDrawList* cmd_list = imDrawData->CmdLists[i];
-            for (int32_t j = 0; j < cmd_list->CmdBuffer.Size; j++)
-            {
-                const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[j];
-                auto scissorX = std::max((int32_t)(pcmd->ClipRect.x), 0);
-                auto scissorY = std::max((int32_t)(pcmd->ClipRect.y), 0);
-                auto scissorWidth = (uint32_t)(pcmd->ClipRect.z - pcmd->ClipRect.x);
-                auto scissorHeight = (uint32_t)(pcmd->ClipRect.w - pcmd->ClipRect.y);
-                renderPassEncoder->setScissor(scissorX, scissorY, scissorWidth, scissorHeight);
-                renderPassEncoder->drawIndexed(pcmd->ElemCount, 1, indexOffset, vertexOffset, 0);
-                indexOffset += pcmd->ElemCount;
-            }
-            vertexOffset += cmd_list->VtxBuffer.Size;
-        }
-    }
-}
-
-void ImGuiSample::clearImGui()
-{
-    ImGui::DestroyContext();
-
-    m_imguiResources.fontSampler.reset();
-    m_imguiResources.fontTextureView.reset();
-    m_imguiResources.fontTexture.reset();
-
-    m_imguiResources.vertexBuffer.reset();
-    m_imguiResources.indexBuffer.reset();
-
-    m_imguiResources.pipeline.reset();
-    m_imguiResources.pipelineLayout.reset();
-    m_imguiResources.bindingGroup.reset();
-    m_imguiResources.bindingGroupLayout.reset();
 }
 
 void ImGuiSample::createDevier()
