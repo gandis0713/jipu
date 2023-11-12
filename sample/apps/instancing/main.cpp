6f518d4a4454cd38350c3c9d093eb433a7067988
gandis0713 <gandis0713@gmail.com>
2023-11-12 17:51:04 +0900

    prepare rotation

--- sample/apps/instancing/main.cpp
+++ sample/apps/instancing/main.cpp
@@ -235,6 +235,18 @@ void InstancingSample::init()
 
 void InstancingSample::updateUniformBuffer()
 {
+    static auto startTime = std::chrono::high_resolution_clock::now();
+
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+    MVP mvp{};
+    mvp.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    mvp.view = m_camera->getViewMat();
+    mvp.proj = m_camera->getProjectionMat();
+
+    void* pointer = m_instancing.mvpUniformBuffer->map();
+    // memcpy(pointer, &mvp, sizeof(MVP));
 }
 
 void InstancingSample::update()
