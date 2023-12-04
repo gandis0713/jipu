bd9b8647f80adaed01d9be4e390baff0fe26255a
gandis0713 <gandis0713@gmail.com>
2023-12-05 00:52:52 +0900

    test copy

--- test/copy_test.cpp
+++ test/copy_test.cpp
@@ -2,13 +2,72 @@
 
 using namespace jipu;
 
-TEST_F(CopyTest, test)
+void CopyTest::SetUp()
 {
-    const uint64_t uniformBufferSize = sizeof(float);
+    WindowTest::SetUp();
 
-    BufferDescriptor uniformBufferDescriptor{};
-    uniformBufferDescriptor.size = uniformBufferSize;
-    uniformBufferDescriptor.usage = BufferUsageFlagBits::kUniform;
-    auto uniformBuffer = m_device->createBuffer(uniformBufferDescriptor);
-    EXPECT_NE(nullptr, uniformBuffer);
+    TextureDescriptor textureDescriptor{};
+    textureDescriptor.type = TextureType::k2D;
+    textureDescriptor.format = TextureFormat::kRGBA_8888_UInt_Norm;
+    textureDescriptor.mipLevels = 1;
+    textureDescriptor.sampleCount = 1;
+    textureDescriptor.width = m_image.width;
+    textureDescriptor.height = m_image.height;
+    textureDescriptor.usage = TextureUsageFlagBits::kCopySrc |
+                              TextureUsageFlagBits::kCopyDst |
+                              TextureUsageFlagBits::kTextureBinding,
+
+    m_imageTexture = m_device->createTexture(textureDescriptor);
+}
+void CopyTest::TearDown()
+{
+    m_imageBuffer.reset();
+    m_imageTexture.reset();
+
+    WindowTest::TearDown();
+}
+
+TEST_F(CopyTest, test_copyTextureToBuffer)
+{
+    BufferDescriptor bufferDescriptor{};
+    bufferDescriptor.size = m_image.width * m_image.height * m_image.channel * sizeof(char);
+    bufferDescriptor.usage = BufferUsageFlagBits::kCopySrc;
+
+    m_imageBuffer = m_device->createBuffer(bufferDescriptor);
+    EXPECT_NE(nullptr, m_imageBuffer);
+    void* imageBufferPointer = m_imageBuffer->map();
+    EXPECT_NE(nullptr, imageBufferPointer);
+    memcpy(imageBufferPointer, m_image.data.data(), bufferDescriptor.size);
+    m_imageBuffer->unmap();
+
+    BlitTextureBuffer blitTextureBuffer{};
+    blitTextureBuffer.buffer = m_imageBuffer.get();
+    blitTextureBuffer.bytesPerRow = m_image.width * m_image.channel * sizeof(char);
+    blitTextureBuffer.rowsPerTexture = m_image.height;
+    blitTextureBuffer.offset = 0;
+
+    BlitTexture blitTexture{};
+    blitTexture.texture = m_imageTexture.get();
+
+    Extent3D extent{};
+    extent.width = m_image.width;
+    extent.height = m_image.height;
+    extent.depth = 1;
+
+    CommandBufferDescriptor commandBufferDescriptor{};
+    commandBufferDescriptor.usage = CommandBufferUsage::kOneTime;
+
+    auto commandBuffer = m_device->createCommandBuffer(commandBufferDescriptor);
+
+    CommandEncoderDescriptor commandEncoderDescriptor{};
+    auto commandEncoder = commandBuffer->createCommandEncoder(commandEncoderDescriptor);
+
+    commandEncoder->copyBufferToTexture(blitTextureBuffer, blitTexture, extent);
+    commandEncoder->finish();
+
+    QueueDescriptor queueDescriptor{};
+    queueDescriptor.flags = QueueFlagBits::kTransfer;
+
+    auto queue = m_device->createQueue(queueDescriptor);
+    queue->submit({ commandBuffer.get() });
 }
--- test/copy_test.h
+++ test/copy_test.h
@@ -1,15 +1,29 @@
 #pragma once
+#include "base/window_test.h"
 
-#if defined(OFFSCREEN_TEST)
-    #include "base/offscreen_test.h"
-#else
-    #include "base/window_test.h"
-#endif
+#include "jipu/texture.h"
 
-#if defined(OFFSCREEN_TEST)
-class CopyTest : public OffscreenTest
-#else
 class CopyTest : public WindowTest
-#endif
 {
+protected:
+    void SetUp() override;
+    void TearDown() override;
+
+protected:
+    std::unique_ptr<jipu::Buffer> m_imageBuffer = nullptr;
+    std::unique_ptr<jipu::Texture> m_imageTexture = nullptr;
+    struct Image
+    {
+        Image()
+            : width(256)
+            , height(256)
+            , channel(4)
+            , data(width * height * channel, static_cast<char>(0xff))
+        {
+        }
+        int width = 0;
+        int height = 0;
+        int channel = 0;
+        std::vector<char> data{};
+    } m_image;
 };
