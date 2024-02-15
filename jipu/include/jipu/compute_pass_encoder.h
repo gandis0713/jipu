d4df92f62347dcab44d22f3eadccd9a3dc19ad8d
gandis0713 <gandis0713@gmail.com>
2024-02-15 22:23:50 +0900

    modify compute pass encoder

--- jipu/include/jipu/compute_pass_encoder.h
+++ jipu/include/jipu/compute_pass_encoder.h
@@ -11,27 +11,17 @@ struct ComputePassDescriptor
 {
 };
 
-class CommandBuffer;
-class Pipeline;
+class ComputePipeline;
 class BindingGroup;
 class ComputePassEncoder
 {
 public:
-    ComputePassEncoder() = delete;
-    ComputePassEncoder(CommandBuffer* commandBuffer, const ComputePassDescriptor& descriptor);
     virtual ~ComputePassEncoder() = default;
 
-    virtual void setPipeline(Pipeline* pipeline) = 0;
+    virtual void setPipeline(ComputePipeline* pipeline) = 0;
     virtual void setBindingGroup(uint32_t index, BindingGroup* bindingGroup, std::vector<uint32_t> dynamicOffset = {}) = 0;
     virtual void dispatch(uint32_t x, uint32_t y = 1, uint32_t z = 1) = 0;
     virtual void end() = 0;
-
-protected:
-    CommandBuffer* m_commandBuffer = nullptr;
-    Pipeline* m_pipeline = nullptr;
-    std::unordered_map<uint32_t, BindingGroup*> m_bindingGroups{};
-
-    ComputePassDescriptor m_descriptor{};
 };
 
 } // namespace jipu
