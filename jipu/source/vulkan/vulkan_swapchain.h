63b29dee1df4da7da12a7402a26d2e4f01dc657e
gandis0713 <gandis0713@gmail.com>
2024-03-10 18:13:35 +0900

    add vulkan swapchain descriptor

--- jipu/source/vulkan/vulkan_swapchain.h
+++ jipu/source/vulkan/vulkan_swapchain.h
@@ -17,12 +17,35 @@ namespace jipu
 
 class VulkanDevice;
 class VulkanTexture;
+class VulkanSurface;
 class VulkanTextureView;
+
+struct VulkanSwapchainDescriptor
+{
+    const void* next = nullptr;
+    VkSwapchainCreateFlagsKHR flags = 0u;
+    VulkanSurface* surface = nullptr;
+    uint32_t minImageCount = 0;
+    VkFormat imageFormat;
+    VkColorSpaceKHR imageColorSpace;
+    VkExtent2D imageExtent;
+    uint32_t imageArrayLayers;
+    VkImageUsageFlags imageUsage;
+    VkSharingMode imageSharingMode;
+    std::vector<uint32_t> queueFamilyIndices{};
+    VkSurfaceTransformFlagBitsKHR preTransform;
+    VkCompositeAlphaFlagBitsKHR compositeAlpha;
+    VkPresentModeKHR presentMode;
+    VkBool32 clipped;
+    VkSwapchainKHR oldSwapchain;
+};
+
 class VULKAN_EXPORT VulkanSwapchain : public Swapchain
 {
 public:
     VulkanSwapchain() = delete;
     VulkanSwapchain(VulkanDevice* device, const SwapchainDescriptor& descriptor) noexcept(false);
+    VulkanSwapchain(VulkanDevice* device, const VulkanSwapchainDescriptor& descriptor) noexcept(false);
     ~VulkanSwapchain() override;
 
     VulkanSwapchain(const Swapchain&) = delete;
@@ -42,17 +65,11 @@ public:
     std::pair<VkSemaphore, VkPipelineStageFlags> getRenderSemaphore() const;
 
 private:
-    VkCompositeAlphaFlagBitsKHR getCompositeAlphaFlagBit(VkCompositeAlphaFlagsKHR supportedCompositeAlpha);
-
-private:
     VulkanDevice* m_device = nullptr;
-    const SwapchainDescriptor m_descriptor{};
+    const VulkanSwapchainDescriptor m_descriptor{};
 
     std::vector<std::unique_ptr<VulkanTexture>> m_textures{};
     std::vector<std::unique_ptr<VulkanTextureView>> m_textureViews{};
-
-    uint32_t m_width = 0;
-    uint32_t m_height = 0;
 
 private:
     VkSwapchainKHR m_swapchain = VK_NULL_HANDLE;
