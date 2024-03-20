240d39a717e4468871619a152bf692d752127109
gandis0713 <gandis0713@gmail.com>
2024-03-21 01:16:17 +0900

    set android sample

--- sample/android/apps/vulkan_pipeline_barrier/src/main/java/com/gandis/jipu/MainActivity.kt
+++ sample/android/apps/vulkan_pipeline_barrier/src/main/java/com/gandis/jipu/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : GameActivity() {
 
     companion object {
         init {
-            System.loadLibrary("vulkan_layout_transition")
+            System.loadLibrary("vulkan_pipeline_barrier")
         }
     }
 }
