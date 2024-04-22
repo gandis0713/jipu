9a5d6c3c17f40935e66b572c7d20cc9fa3cdc290
gandis0713 <gandis0713@gmail.com>
2024-04-22 15:40:28 +0900

    prepare gpu counter

---
+++ sample/utils/AndroidGPUCounter.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GPUCounter.h"
+
+namespace jipu
+{
+namespace sample
+{
+namespace utils
+{
+
+class AndroidGPUCounter : public GPUCounter
+{
+};
+
+} // namespace utils
+
+} // namespace sample
+} // namespace jipu
---
+++ sample/utils/GPUCounter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace jipu
+{
+namespace sample
+{
+namespace utils
+{
+
+class GPUCounter
+{
+};
+
+} // namespace utils
+} // namespace sample
+} // namespace jipu
