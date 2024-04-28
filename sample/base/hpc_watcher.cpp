5f101d91a2dd1134429afa93757918dfe2b7846a
gandis0713 <gandis0713@gmail.com>
2024-04-29 01:06:35 +0900

    hpc watcher

--- sample/base/hpc_watcher.cpp
+++ sample/base/hpc_watcher.cpp
@@ -1,13 +1,17 @@
 #include "hpc_watcher.h"
 
+#include <spdlog/spdlog.h>
+
 using namespace std::chrono;
 
 namespace jipu
 {
 
-HPCWatcher::HPCWatcher()
-    : m_time(duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()))
+HPCWatcher::HPCWatcher(hpc::Sampler::Ptr sampler)
+    : m_sampler(std::move(sampler))
+    , m_time(duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()))
 {
+    spdlog::error("charles {}", __func__);
 }
 
 void HPCWatcher::update()
--- sample/base/hpc_watcher.h
+++ sample/base/hpc_watcher.h
@@ -2,19 +2,27 @@
 
 #include <chrono>
 
+#include "hpc/sampler.h"
+
 namespace jipu
 {
 
 class HPCWatcher
 {
 public:
-    HPCWatcher();
+    HPCWatcher(hpc::Sampler::Ptr sampler);
 
     void update();
 
 private:
+    hpc::Sampler::Ptr m_sampler{ nullptr };
+
+private:
     uint32_t period = 1000; // 1 second
     std::chrono::milliseconds m_time = std::chrono::milliseconds::zero();
+
+public:
+    using Ptr = std::unique_ptr<HPCWatcher>;
 };
 
 } // namespace jipu
