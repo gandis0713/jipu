0e9e72a1815b9b2e6b1ffecede47046e6ce7e29f
gandis0713 <gandis0713@gmail.com>
2024-06-30 22:16:04 +0900

    change syscall interface

--- sample/base/hpc/backend/device/source/syscall/interface.h
+++ sample/base/hpc/backend/device/source/syscall/interface.h
@@ -15,19 +15,19 @@ namespace device
 {
 namespace syscall
 {
+
 namespace impl
 {
-
-template <class T>
+template <class SyscallAPI_T>
 class Interface
 {
 private:
-    using SyscallAPI_T = T;
+    using SyscallAPI = SyscallAPI_T;
 
 public:
     static std::pair<std::error_code, int> open(const char* name, int oflags)
     {
-        const int fd = SyscallAPI_T::open(name, oflags);
+        const int fd = SyscallAPI::open(name, oflags);
 
         std::error_code ec;
         if (fd < 0)
@@ -38,7 +38,7 @@ public:
 
     static std::error_code close(int fd)
     {
-        const int result = SyscallAPI_T::close(fd);
+        const int result = SyscallAPI::close(fd);
 
         std::error_code ec;
         if (result < 0)
@@ -47,10 +47,10 @@ public:
         return ec;
     }
 
-    template <typename command_t, typename... args_t>
-    static std::pair<std::error_code, int> ioctl(int fd, command_t command, args_t&&... args)
+    template <typename Command, typename... Args>
+    static std::pair<std::error_code, int> ioctl(int fd, Command command, Args&&... args)
     {
-        const int result = SyscallAPI_T::ioctl(fd, command, std::forward<args_t>(args)...);
+        const int result = SyscallAPI::ioctl(fd, command, std::forward<Args>(args)...);
 
         std::error_code ec;
         if (result < 0)
@@ -61,7 +61,7 @@ public:
 
     static std::pair<std::error_code, int> poll(struct pollfd* fds, nfds_t nfds, int timeout)
     {
-        const int result = SyscallAPI_T::poll(fds, nfds, timeout);
+        const int result = SyscallAPI::poll(fds, nfds, timeout);
 
         std::error_code ec;
 
