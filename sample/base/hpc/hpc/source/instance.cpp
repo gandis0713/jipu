197a39c2e9c292a2033b45bb68cf82fb0a4f7d9b
gandis0713 <gandis0713@gmail.com>
2024-06-30 22:40:44 +0900

    create adreno instance

--- sample/base/hpc/hpc/source/instance.cpp
+++ sample/base/hpc/hpc/source/instance.cpp
@@ -3,6 +3,7 @@
 #include "hpc/gpu.h"
 
 #if defined(__ANDROID__) || defined(ANDROID) || defined(__linux__)
+#include "adreno/adreno_instance.h"
 #include "mali/mali_instance.h"
 #endif
 
@@ -16,9 +17,10 @@ std::unique_ptr<Instance> Instance::create(const InstanceDescriptor& descriptor)
 #if defined(__ANDROID__) || defined(ANDROID) || defined(__linux__)
     case GPUType::Mali:
         return std::make_unique<mali::MaliInstance>();
-#endif
     case GPUType::Adreno:
         return nullptr;
+#endif
+
     default:
         return nullptr;
     }
