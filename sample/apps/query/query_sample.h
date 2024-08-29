c91ddadbd22d12166c1259e6bf097107d3d9d3f0
gandis0713 <gandis0713@gmail.com>
2024-08-29 09:51:22 +0900

    leave comment

--- sample/apps/query/query_sample.h
+++ sample/apps/query/query_sample.h
@@ -56,8 +56,8 @@ private:
     std::unique_ptr<Buffer> m_vertexBuffer = nullptr;
     std::unique_ptr<Buffer> m_indexBuffer = nullptr;
     std::unique_ptr<Buffer> m_uniformBuffer = nullptr;
-    std::unique_ptr<Buffer> m_timestampQueryBuffer = nullptr;
-    std::unique_ptr<Buffer> m_occlusionQueryBuffer = nullptr;
+    std::unique_ptr<Buffer> m_timestampQueryBuffer = nullptr; // use same buffer with occlusion
+    std::unique_ptr<Buffer> m_occlusionQueryBuffer = nullptr; // use same buffer with timestamp
     std::unique_ptr<BindingGroupLayout> m_bindingGroupLayout = nullptr;
     std::unique_ptr<BindingGroup> m_bindingGroup = nullptr;
     std::unique_ptr<PipelineLayout> m_renderPipelineLayout = nullptr;
