2ceeb2e8d6b2cb945a72de973dea2fedfc449380
gandis0713 <gandis0713@gmail.com>
2024-09-02 17:34:47 +0900

    only support metal layer

--- jipu/source/vulkan/vulkan_surface_macos.mm
+++ jipu/source/vulkan/vulkan_surface_macos.mm
@@ -18,18 +18,7 @@ VulkanSurfaceDescriptor generateVulkanSurfaceDescriptor(const SurfaceDescriptor&
     {
         VulkanSurfaceDescriptor vkdescriptor{};
 
-        NSView* nsView = (__bridge NSView*)descriptor.windowHandle;
-        if (nsView == nil)
-        {
-            throw std::runtime_error(fmt::format("[{}] Failed to get NSView.", __func__));
-        }
-
-        NSBundle* bundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/QuartzCore.framework"];
-        CAMetalLayer* layer = [[bundle classNamed:@"CAMetalLayer"] layer];
-        [nsView setLayer:layer];
-        [nsView setWantsLayer:YES];
-
-        vkdescriptor.layer = layer;
+        vkdescriptor.layer = (__bridge CAMetalLayer*)descriptor.windowHandle;
 
         return vkdescriptor;
     }
@@ -50,61 +39,61 @@ void VulkanSurface::createSurfaceKHR()
 }
 #elif defined(VK_USE_PLATFORM_MACOS_MVK)
 
-VulkanSurfaceDescriptor generateVulkanSurfaceDescriptor(const SurfaceDescriptor& descriptor)
-{
-    @autoreleasepool
-    {
-        VulkanSurfaceDescriptor vkdescriptor{};
+// VulkanSurfaceDescriptor generateVulkanSurfaceDescriptor(const SurfaceDescriptor& descriptor)
+// {
+//     @autoreleasepool
+//     {
+//         VulkanSurfaceDescriptor vkdescriptor{};
 
-        NSView* nsView = (__bridge NSView*)descriptor.windowHandle;
-        if (nsView == nil)
-        {
-            throw std::runtime_error(fmt::format("[{}] Failed to get NSView.", __func__));
-        }
-        NSBundle* bundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/QuartzCore.framework"];
-        CAMetalLayer* layer = [[bundle classNamed:@"CAMetalLayer"] layer];
+//         NSView* nsView = (__bridge NSView*)descriptor.windowHandle;
+//         if (nsView == nil)
+//         {
+//             throw std::runtime_error(fmt::format("[{}] Failed to get NSView.", __func__));
+//         }
+//         NSBundle* bundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/QuartzCore.framework"];
+//         CAMetalLayer* layer = [[bundle classNamed:@"CAMetalLayer"] layer];
 
-        [nsView setLayer:layer];
+//         [nsView setLayer:layer];
 
-        vkdescriptor.view = (__bridge void*)nsView;
+//         vkdescriptor.view = (__bridge void*)nsView;
 
-        return vkdescriptor;
-    }
-}
+//         return vkdescriptor;
+//     }
+// }
 
-void VulkanSurface::createSurfaceKHR()
-{
-    @autoreleasepool
-    {
-        NSView* nsView = (__bridge NSView*)m_descriptor.windowHandle;
-        if (nsView == nil)
-        {
-            throw std::runtime_error(fmt::format("[{}] Failed to get NSView.", __func__));
-        }
-        NSBundle* bundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/QuartzCore.framework"];
-        CAMetalLayer* layer = [[bundle classNamed:@"CAMetalLayer"] layer];
+// void VulkanSurface::createSurfaceKHR()
+// {
+//     @autoreleasepool
+//     {
+//         NSView* nsView = (__bridge NSView*)m_descriptor.windowHandle;
+//         if (nsView == nil)
+//         {
+//             throw std::runtime_error(fmt::format("[{}] Failed to get NSView.", __func__));
+//         }
+//         NSBundle* bundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/QuartzCore.framework"];
+//         CAMetalLayer* layer = [[bundle classNamed:@"CAMetalLayer"] layer];
 
-        [nsView setLayer:layer];
+//         [nsView setLayer:layer];
 
-        VkMacOSSurfaceCreateInfoMVK createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_MACOS_SURFACE_CREATE_INFO_MVK;
-        createInfo.pView = (__bridge void*)nsView;
+//         VkMacOSSurfaceCreateInfoMVK createInfo{};
+//         createInfo.sType = VK_STRUCTURE_TYPE_MACOS_SURFACE_CREATE_INFO_MVK;
+//         createInfo.pView = (__bridge void*)nsView;
 
-        VulkanInstance& instance = downcast(m_instance);
-        const VulkanAPI& vkAPI = instance.vkAPI;
-        if (vkAPI.CreateMacOSSurfaceMVK == nullptr)
-        {
-            throw std::runtime_error("vkCreateMacOSSurfaceMVK is nullptr.");
-        }
+//         VulkanInstance& instance = downcast(m_instance);
+//         const VulkanAPI& vkAPI = instance.vkAPI;
+//         if (vkAPI.CreateMacOSSurfaceMVK == nullptr)
+//         {
+//             throw std::runtime_error("vkCreateMacOSSurfaceMVK is nullptr.");
+//         }
 
-        VkResult result = vkAPI.CreateMacOSSurfaceMVK(instance.getVkInstance(), &createInfo, nullptr, &m_surface);
+//         VkResult result = vkAPI.CreateMacOSSurfaceMVK(instance.getVkInstance(), &createInfo, nullptr, &m_surface);
 
-        if (result != VK_SUCCESS)
-        {
-            throw std::runtime_error(fmt::format("Failed to create VkSurfaceKHR.: {}", static_cast<int32_t>(result)));
-        }
-    }
-}
+//         if (result != VK_SUCCESS)
+//         {
+//             throw std::runtime_error(fmt::format("Failed to create VkSurfaceKHR.: {}", static_cast<int32_t>(result)));
+//         }
+//     }
+// }
 
 #endif
 
