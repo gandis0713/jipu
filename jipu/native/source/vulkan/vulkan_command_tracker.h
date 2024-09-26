b70fb3ea88ba1b10c46b183795a1455479bb5e35
gandis0713 <gandis0713@gmail.com>
2024-09-26 19:09:53 +0900

    command tracker

---
+++ jipu/native/source/vulkan/vulkan_command_tracker.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "vulkan_api.h"
+#include "vulkan_command.h"
+
+namespace jipu
+{
+
+class VulkanCommandTracker final
+{
+public:
+    VulkanCommandTracker() = default;
+    ~VulkanCommandTracker() = default;
+
+public:
+    // compute pass
+    void beginComputePass(BeginComputePassCommand* command);
+    void setComputePipeline(SetComputePipelineCommand* command);
+    void setComputeBindingGroup(SetBindGroupCommand* command);
+    void dispatch(DispatchCommand* command);
+    void dispatchIndirect(DispatchIndirectCommand* command);
+    void endComputePass(EndComputePassCommand* command);
+
+    // render pass
+    void beginRenderPass(BeginRenderPassCommand* command);
+    void setRenderPipeline(SetRenderPipelineCommand* command);
+    void setRenderBindingGroup(SetBindGroupCommand* command);
+    void setVertexBuffer(SetVertexBufferCommand* command);
+    void setIndexBuffer(SetIndexBufferCommand* command);
+    void setViewport(SetViewportCommand* command);
+    void setScissor(SetScissorCommand* command);
+    void setBlendConstant(SetBlendConstantCommand* command);
+    void draw(DrawCommand* command);
+    void drawIndexed(DrawIndexedCommand* command);
+    void beginOcclusionQuery(BeginOcclusionQueryCommand* command);
+    void endOcclusionQuery(EndOcclusionQueryCommand* command);
+    void endRenderPass(EndRenderPassCommand* command);
+
+    // copy
+    void copyBufferToBuffer(CopyBufferToBufferCommand* command);
+    void copyBufferToTexture(CopyBufferToTextureCommand* command);
+    void copyTextureToBuffer(CopyTextureToBufferCommand* command);
+    void copyTextureToTexture(CopyTextureToTextureCommand* command);
+
+    // query
+    void resolveQuerySet(ResolveQuerySetCommand* command);
+};
+
+} // namespace jipu
