2428a99348fdbbc01ddda56da8f539e4cac33335
gandis0713 <gandis0713@gmail.com>
2024-09-30 08:58:02 +0900

    init command buffer tracker

--- jipu/native/source/vulkan/vulkan_command_buffer_tracker.h
+++ jipu/native/source/vulkan/vulkan_command_buffer_tracker.h
@@ -1,49 +1,34 @@
 #pragma once
 
 #include "vulkan_api.h"
-#include "vulkan_command.h"
+#include "vulkan_command_recorder.h"
 
 namespace jipu
 {
+
+struct VulkanCommandBufferTrackerDescriptor
+{
+    std::vector<std::pair<CommandBuffer*, VulkanCommandRecordResult>> commandRecordResults{};
+};
+
+struct CommandBufferUsageInfo
+{
+    VkPipelineStageFlags srcStageFlags = 0u;
+    VkPipelineStageFlags dstStageFlags = 0u;
+};
+struct CommandBufferTrackingResult
+{
+    std::vector<std::pair<CommandBuffer*, CommandBufferUsageInfo>> commandRecordResults{};
+};
 
 class VulkanCommandBufferTracker final
 {
 public:
     VulkanCommandBufferTracker() = default;
+    VulkanCommandBufferTracker(const VulkanCommandBufferTrackerDescriptor& descriptor);
     ~VulkanCommandBufferTracker() = default;
 
 public:
-    // compute pass
-    void beginComputePass(BeginComputePassCommand* command);
-    void setComputePipeline(SetComputePipelineCommand* command);
-    void setComputeBindingGroup(SetBindGroupCommand* command);
-    void dispatch(DispatchCommand* command);
-    void dispatchIndirect(DispatchIndirectCommand* command);
-    void endComputePass(EndComputePassCommand* command);
-
-    // render pass
-    void beginRenderPass(BeginRenderPassCommand* command);
-    void setRenderPipeline(SetRenderPipelineCommand* command);
-    void setRenderBindingGroup(SetBindGroupCommand* command);
-    void setVertexBuffer(SetVertexBufferCommand* command);
-    void setIndexBuffer(SetIndexBufferCommand* command);
-    void setViewport(SetViewportCommand* command);
-    void setScissor(SetScissorCommand* command);
-    void setBlendConstant(SetBlendConstantCommand* command);
-    void draw(DrawCommand* command);
-    void drawIndexed(DrawIndexedCommand* command);
-    void beginOcclusionQuery(BeginOcclusionQueryCommand* command);
-    void endOcclusionQuery(EndOcclusionQueryCommand* command);
-    void endRenderPass(EndRenderPassCommand* command);
-
-    // copy
-    void copyBufferToBuffer(CopyBufferToBufferCommand* command);
-    void copyBufferToTexture(CopyBufferToTextureCommand* command);
-    void copyTextureToBuffer(CopyTextureToBufferCommand* command);
-    void copyTextureToTexture(CopyTextureToTextureCommand* command);
-
-    // query
-    void resolveQuerySet(ResolveQuerySetCommand* command);
 };
 
 } // namespace jipu
